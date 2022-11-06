eb26cec831075a33aa65f48bbb4c92caebac91d2
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-06 13:19:28 +0200

    add solution to Aminer task

---
+++ E25. Dictionaries/P02 A miner task/AMinerTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AMinerTask
+{
+    static void Main()
+    {
+        Dictionary<string, int> resources = new Dictionary<string, int>();
+
+        string resource = Console.ReadLine();
+        while (resource != "stop")
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            if (!resources.ContainsKey(resource))
+            {
+                resources.Add(resource, 0);
+            }
+            resources[resource] += quantity;
+
+            resource = Console.ReadLine();
+        }
+        foreach (var item in resources)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}

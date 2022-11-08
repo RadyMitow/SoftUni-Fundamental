5603eead1e3bd21f2e058733cb5ad2847d7dbfde
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-08 20:22:44 +0200

    add solution for Company users

---
+++ E25. Dictionaries/P08 Company Users/CompanyUsers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CompanyUsers
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+        string commnad = Console.ReadLine();
+        while (commnad != "End")
+        {
+            string[] commandInfo = commnad.Split(' ').ToArray();
+            string company = commandInfo[0];
+            string employee = commandInfo[2];
+
+            if (!companies.ContainsKey(company))
+            {
+                companies.Add(company, new List<string>());
+            }
+            if (!companies[company].Contains(employee))
+            {
+                companies[company].Add(employee);
+            }
+
+            commnad = Console.ReadLine();
+        }
+
+        foreach (var item in companies)
+        {
+            Console.WriteLine(item.Key);
+            foreach (var employee in item.Value)
+            {
+                Console.WriteLine($"-- {employee}");
+            }
+
+        }
+    }
+}

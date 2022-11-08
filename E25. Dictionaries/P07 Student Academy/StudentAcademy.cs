5cea9cf5009c5431cbcc9960a645543ae7621b26
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-08 19:53:59 +0200

    add solution to Student Academy

---
+++ E25. Dictionaries/P07 Student Academy/StudentAcademy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentAcademy
+{
+    static void Main()
+    {
+        Dictionary<string, List<double>> students = new Dictionary<string, List<double> >();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string student = Console.ReadLine();
+            double grade = double.Parse(Console.ReadLine());
+            if (!students.ContainsKey(student))
+            {
+                students.Add(student, new List<double>());
+            }
+            students[student].Add(grade);
+        }
+        foreach (var item in students)
+        {
+            if (item.Value.Average() < 4.50)
+            {
+                students.Remove(item.Key);
+            }
+
+        }       
+        
+        foreach (var item in students)
+        {
+            double avgGrade = item.Value.Average();
+            Console.WriteLine($"{item.Key} -> {avgGrade:F2}");
+        }
+    }
+}

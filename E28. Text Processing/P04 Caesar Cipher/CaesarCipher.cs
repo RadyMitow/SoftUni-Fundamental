9ee82c16aa1c27b4ea3d2a792a594ee85ed3ecd9
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-14 21:06:47 +0200

    add solution for Ceasar Cipher

---
+++ E28. Text Processing/P04 Caesar Cipher/CaesarCipher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+class CaesarCipher
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            int temp = 0;
+
+            temp +=input[i] + 3;
+            sb.Append((char)(temp));
+            
+
+        }
+        string cipher = sb.ToString();
+        Console.WriteLine(cipher);
+
+    }
+}

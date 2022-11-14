c83783fbeaba1a70566dc0ff31c27dcb9fc53961
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-14 20:32:28 +0200

    add solution for Extract File

---
+++ E28. Text Processing/P03 Extract File/ExtractFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ExtractFile
+{
+    static void Main()
+    {
+        string path = Console.ReadLine();
+        string file = path.Substring(path.LastIndexOf('\\') +1, path.Length-path.LastIndexOf('\\')-1);
+        string fileName = file.Substring(0, file.IndexOf('.'));
+        string fileExtension = file.Substring(file.IndexOf('.')+1, file.Length - file.IndexOf('.')-1);
+
+        Console.WriteLine($"File name: {fileName}");
+        Console.WriteLine($"File extension: { fileExtension}");
+    }
+}

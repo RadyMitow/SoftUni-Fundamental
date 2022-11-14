38157410c6da7cd140a1486e354177a73f8601e2
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-14 20:12:21 +0200

    add solution for Charecter Multiplier

---
+++ E28. Text Processing/P02 Character Multiplier/CharecterMultiplier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class CharecterMultiplier
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string firstString = input.Split(' ').ToArray()[0];
+        string secondString = input.Split(' ').ToArray()[1];
+
+        Console.WriteLine(MultiplyTwoStrings(firstString, secondString));
+
+        int MultiplyTwoStrings(string first, string second)
+        {
+            int sum = 0;
+            int minLength = Math.Min(first.Length, second.Length);
+            int maxLength = Math.Max(first.Length, second.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < minLength)
+                {
+                    sum += first[i] * second[i];
+                }
+                else if (first.Length == minLength)
+                {
+                    sum += second[i];
+                }
+                else
+                {
+                    sum += first[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+
+}

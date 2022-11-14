a64b183f5231b8f88f0a9419495b6bfb6034fe9f
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-11-14 22:31:38 +0200

    Add solution for Multiply Big Number

---
+++ E28. Text Processing/P05 Multiply Big Number/MultiplyBigNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class MultiplyBigNumber
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        int multiplier = int.Parse(Console.ReadLine());
+        StringBuilder sb = new StringBuilder();
+        int temp = 0;
+
+        if (number == "0" || multiplier== 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        for (int i = number.Length-1; i >= 0; i--)
+        {
+            int digit = int.Parse(number[i].ToString());
+            int result = (digit * multiplier);
+            if (result + temp < 10 )
+            {
+                result += temp;
+                temp = 0;
+                sb.Append(result.ToString());
+            }
+            else 
+            {
+                result += temp;
+                temp = result / 10;
+                sb.Append((result % 10).ToString());
+            }
+        }
+        if (temp>0)
+        {
+            sb.Append(temp.ToString());
+        }
+        List<char> product = sb.ToString().ToList();
+        product.Reverse();
+
+        foreach (var charecter in product)
+        {
+            Console.Write(charecter);
+        }
+    }
+}

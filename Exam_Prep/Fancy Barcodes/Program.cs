a595e84ccf2984c90d995b0c082d40ba07e3778a
Plamen Dmitrov <dimitrov.plam@gmail.com>
2022-12-04 13:13:57 +0200

    add 50/100 solution for Fancy Barcodes

---
+++ Exam_Prep/Fancy Barcodes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@#+[A-z][A-Za-z0-9]{4,}[A-z]@#+";
+            Regex barCodeRegex = new Regex(pattern);
+            Regex productGroupRegex = new Regex(@"\d+");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+                var match = barCodeRegex.Match(barcode);
+                if (match.Success)
+                {
+                    MatchCollection productGroupMatch = productGroupRegex.Matches(match.Value);
+                    if (productGroupMatch.Count > 0)
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", productGroupMatch)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+        }
+    }
+}
